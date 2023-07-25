6e222d2faa40fae61a3791958b6913a1115057cb
afolabi mabunmi <brownboy3047@gmail.com>
2023-07-25 17:32:51 +0100

    refactor some code

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import "./App.css";
 
 function App() {
   return (
-    <main className="app">
+    <div className="app">
       <BrowserRouter>
         <NavBar />
         <Routes>
@@ -29,7 +29,7 @@ function App() {
 
         <Footer />
       </BrowserRouter>
-    </main>
+    </div>
   );
 }
 
